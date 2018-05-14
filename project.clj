4e476fb04df205d53ac38e0e94718cf09fd29317
冯忠孝 <32647908@qq.com>
2018-05-14 11:19:56 +0800

    use pr-str to log lazseq

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject org.clojure/tools.logging "0.4.666"
+(defproject org.clojure/tools.logging "0.4.667"
   :description "Clojure logging API."
   :url "https://github.com/clojure/tools.logging"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
+  :repositories [["releases" {:url "http://repocloudvastyun.cloudvast.net/repo/" :sign-releases false :checksum :fail :update :always :username "feng" :password "fengcloudvast" :releases {:checksum :fail :update :always}}]]
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :profiles {:test {:dependencies [[org.slf4j/slf4j-api "1.7.25"]
                                    [org.slf4j/slf4j-log4j12 "1.7.25"]
